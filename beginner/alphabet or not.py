c6e303bb4893f88195001118ee56254333fb739d
jayaseelan2110 <36723440+jayaseelan2110@users.noreply.github.com>
2018-02-23 10:34:05 +0530

    Create alphabet or not.py

---
+++ beginner/alphabet or not.py
@@ -0,0 +1,9 @@
+def main():
+ n=str(input())
+ if(((n>='a')and(n<='z'))or((n>='A')and(n<='Z'))):
+    print("character is alphabet")
+ else:
+    print("character is not alphabet")
+
+if __name__ == '__main__':
+    main()
