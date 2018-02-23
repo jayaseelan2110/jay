084df5fa4ca1afb3ca69b0b2745452bd97cd6f53
jayaseelan2110 <36723440+jayaseelan2110@users.noreply.github.com>
2018-02-23 10:13:45 +0530

    Create vowel or consonent.py

---
+++ beginner/vowel or consonent.py
@@ -0,0 +1,9 @@
+def main():
+  n=str(input())
+  if((n=='a')or(n=='e')or(n=='i')or(n=='o')or(n=='u')or(n=='A')or(n=='E')or(n=='I')or(n=='O')or(n=='U')):
+    print("character is vowel")
+  else:
+    print("character is consonent")
+
+if __name__ == '__main__':
+    main()
